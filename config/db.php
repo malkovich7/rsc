eeff9c45ca710712a68def0312fcfbd936ad4e6e
malkovich <mjcruz@upax.com.mx>
2019-07-07 22:05:06 -0500

    [UPDATE] Cambio de accesos a BD

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=sebuscanmascotas.com;dbname=sebusca1_rsc',
-    'username' => 'sebusca1_rsc',
-    'password' => 'X17KyQQ0w0tw',
+    'dsn' => 'mysql:host=rsc.c7b3pqsajrsn.us-east-1.rds.amazonaws.com;dbname=rsc_db',
+    'username' => 'user_rsc',
+    'password' => 'rsc_2019',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
